8d5b03d71e78bd56a403fb0a0c9c2915ce443072
Hidde de Vries <hidde@hiddedevries.nl>
2017-11-28 11:08:14 +0100

    add LDAP and loading button style
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/authorise-ldap.js
+++ src/js/handlers/authorise-ldap.js
@@ -1,8 +1,16 @@
 module.exports = function authorise( element ) {
   var form = element.closest( 'form' );
+  var emailField = document.getElementById( 'field-email' );
+  var passwordField = document.getElementById( 'field-password' );
+  var button = document.getElementById( 'authorise-ldap-credentials' );
 
-  form.webAuth.authorize({
-    responseType: 'code',
-    redirectUri: 'http://localhost:8080/callback'
+  form.webAuth.redirect.loginWithCredentials({
+    connection: 'Username-Password-Authentication',
+    username: emailField.value,
+    password: passwordField.value,
+    redirectUri: 'http://localhost:3000',
+    scope: 'openid'
+  }, function(err, authResult) {
+    button.classList.add( 'button--loading' );
   });
 }

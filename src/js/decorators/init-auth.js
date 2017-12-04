d389ced4a46be2d723f0d9be3ade686550911df2
Hidde de Vries <hidde@hiddedevries.nl>
2017-12-04 21:32:16 +0100

    remove log
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/init-auth.js
+++ src/js/decorators/init-auth.js
@@ -28,8 +28,6 @@ function getConfig( string ) {
 module.exports = function initAuth( element ) {
   var auth0 = require( 'auth0-js' );
   var config = getConfig( '@@config@@' );
-
-  console.log( config );
   var webAuth = new auth0.WebAuth( config );
 
   element.webAuth = webAuth;

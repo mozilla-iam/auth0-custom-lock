342a0ca09f886fe077c50bf8fd87b072a182c167
Hidde de Vries <hidde@hiddedevries.nl>
2017-12-04 15:55:29 +0100

    add passwordless success message
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/send-passwordless-link.js
+++ src/js/handlers/send-passwordless-link.js
@@ -3,10 +3,23 @@ var ui = require( 'helpers/ui' );
 module.exports = function authorise( element ) {
   var emailField = document.getElementById( 'field-email' );
   var form = element.form;
+  var emailPlaceholder = document.getElementById( 'passwordless-success-email-address' );
+
+  ui.setLockState( element, 'loading' );
 
   form.webAuth.passwordlessStart({
     connection: 'email',
     send: 'link',
+    responseType: 'token',
     email: emailField.value
+  }, function( error, response ) {
+    if ( error ) {
+      console.log( error );
+      ui.setLockState( element, 'non-ldap' );
+    }
+    if ( response ) {
+      emailPlaceholder.textContent = emailField.value;
+      ui.setLockState( element, 'passwordless-success' );
+    }
   });
 }

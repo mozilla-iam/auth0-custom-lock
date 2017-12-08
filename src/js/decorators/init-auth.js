219ac00ad526039bb7558fb194461d20c48f2b87
Hidde de Vries <hidde@hiddedevries.nl>
2017-12-08 11:43:18 +0100

    pass internalOptions in to make hosted lock work
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/init-auth.js
+++ src/js/decorators/init-auth.js
@@ -21,7 +21,13 @@ function getConfig( string ) {
 module.exports = function initAuth( element ) {
   var auth0 = require( 'auth0-js' );
   var config = getConfig( '@@config@@' );
-  var webAuth = new auth0.WebAuth( config );
+  var params = Object.assign( {
+    domain: config.auth0Domain,
+    clientID: config.clientID,
+    redirectUri: config.callbackURL,
+    responseType: 'code'
+  }, config.internalOptions );
+  var webAuth = new auth0.WebAuth( params );
 
   element.webAuth = webAuth;
   element.webAuthConfig = config;

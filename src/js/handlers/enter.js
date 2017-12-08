6c7be44afba95378abf3d0ff94b594c28a5cc87c
Hidde de Vries <hidde@hiddedevries.nl>
2017-12-08 15:39:51 +0100

    add basic email validation
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/enter.js
+++ src/js/handlers/enter.js
@@ -5,6 +5,11 @@ module.exports = function enter( element ) {
   var form = element.form;
   var emailField = document.getElementById( 'field-email' );
 
+  if ( emailField.value === '' || emailField.validity.valid === false ) {
+    emailField.focus();
+    return
+  }
+
   if ( isLDAP( emailField.value ) ) {
     // show password field
     ui.setLockState( element, 'ldap' );

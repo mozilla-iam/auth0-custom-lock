bd9f11283a39d77366b6955d32e1ba6bb8cebaf2
Hidde de Vries <hidde@hiddedevries.nl>
2017-12-12 23:03:23 -0600

    focus password field when it opens
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/enter.js
+++ src/js/handlers/enter.js
@@ -4,6 +4,7 @@ var ui = require( 'helpers/ui' );
 module.exports = function enter( element ) {
   var form = element.form;
   var emailField = document.getElementById( 'field-email' );
+  var passwordField = document.getElementById( 'field-password' );
 
   if ( emailField.value === '' || emailField.validity.valid === false ) {
     emailField.focus();
@@ -13,6 +14,10 @@ module.exports = function enter( element ) {
   if ( isLDAP( emailField.value ) ) {
     // show password field
     ui.setLockState( element, 'ldap' );
+    // focus password field
+    setTimeout( function() {
+      passwordField.focus();
+    }, 400 );
   }
   else {
     // show social logins + passwordless

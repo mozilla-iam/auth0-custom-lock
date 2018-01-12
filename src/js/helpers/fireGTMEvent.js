357d1896c87abc2caf6a212f366a08c3ee1aa71e
Hidde de Vries <hidde@hiddedevries.nl>
2018-01-12 09:50:41 +0100

    Send events to GA not GTM; send RP name
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/helpers/fireGTMEvent.js
+++ src/js/helpers/fireGTMEvent.js
@@ -1,7 +1,14 @@
-module.exports = function( eventName ) {
-  if ( window.dataLayer ) {
-    dataLayer.push({
-      'event': eventName
+module.exports = function( eventCategory, eventAction ) {
+  var TRACKER_NAME = 'gtm1';
+  var getRPName = require( 'helpers/getRPName' );
+  var RP = getRPName();
+
+  if ( window.ga ) {
+    ga( TRACKER_NAME + '.send', {
+      'hitType': 'event',
+      'eventCategory': eventCategory,
+      'eventAction': eventAction,
+      'eventLabel': RP
     });
   }
 };
---
+++ src/js/helpers/getRPName.js
@@ -0,0 +1,36 @@
+function getUrlParameter( name ) {
+  var regex;
+  var results;
+
+  name = name.replace( /[\[]/, '\\[' ).replace( /[\]]/, '\\]' );
+  regex = new RegExp( '[\\?&]' + name + '=([^&#]*)' );
+  results = regex.exec( location.search );
+
+  return results === null ? '' : decodeURIComponent( results[1].replace( /\+/g, ' ' ) );
+}
+
+// via https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
+function extractHostname( url ) {
+  var hostname;
+
+  // find & remove protocol (http, ftp, etc.) and get hostname
+  if ( url.indexOf( '://' ) > -1 ) {
+    hostname = url.split( '/' )[2];
+  }
+  else {
+    hostname = url.split( '/' )[0];
+  }
+
+  // find & remove port number
+  hostname = hostname.split( ':' )[0];
+  // find & remove "?"
+  hostname = hostname.split( '?' )[0];
+
+  return hostname;
+}
+
+module.exports = function() {
+  var RP = getUrlParameter( 'redirect_uri' ) || '';
+
+  return extractHostname( RP );
+}

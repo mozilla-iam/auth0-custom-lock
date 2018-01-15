5455457c9b4ed2be37751af4cbbf02928b88593e
Hidde de Vries <hidde@hiddedevries.nl>
2018-01-15 13:48:43 +0100

    Display RP name first instead of last
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/display-rp-name.js
+++ src/js/decorators/display-rp-name.js
@@ -2,5 +2,5 @@ module.exports = function() {
   var getRPName = require( 'helpers/getRPName' );
   var rp = getRPName();
 
-  document.title = document.title + ' - ' + rp;
+  document.title = rp + ' - ' + document.title;
 };

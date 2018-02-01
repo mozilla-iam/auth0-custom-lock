f4cd658b249499143e3145107e4e4e17c2ce6dea
Hidde de Vries <hidde@hiddedevries.nl>
2018-02-01 12:51:41 +0100

    Hide login options that are unusable for RP
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators.js
+++ src/js/decorators.js
@@ -8,7 +8,8 @@ var decorators = {
   'auto-login': require( 'decorators/auto-login' ),
   'load-ga': require( 'decorators/load-ga' ),
   'handle-submit': require( 'decorators/handle-submit' ),
-  'display-rp-name': require( 'decorators/display-rp-name' )
+  'display-rp-name': require( 'decorators/display-rp-name' ),
+  'filter-connections': require( 'decorators/filter-connections' )
 };
 
 module.exports = decorators;
---
+++ src/js/decorators/filter-connections.js
@@ -0,0 +1,25 @@
+var dom = require( 'helpers/dom' );
+var ui  = require( 'helpers/ui' );
+
+module.exports = function( form ) {
+  var url = 'https://auth-dev.mozilla.auth0.com/public/api/' + form.webAuthConfig.clientID + '/connections';
+
+  fetch( url ).then( function( response ) {
+    return response.json()
+  }).then( function( allowed ) {
+    var allowedRPs = [];
+    var RPfunctionalities = dom.$( '[data-optional-rp]' );
+
+    for ( var i = 0; i < allowed.length; i++) {
+      allowedRPs.push( allowed[i].name );
+    }
+
+    RPfunctionalities.forEach( function( functionality ) {
+      var functionalityName = functionality.getAttribute( 'data-optional-rp' );
+
+      if ( allowedRPs.indexOf( functionalityName ) === -1 ) {
+        ui.hide( functionality );
+      }
+    });
+  });
+};

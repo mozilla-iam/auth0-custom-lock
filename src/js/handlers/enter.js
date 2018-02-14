7e15a566d7eda101d656b8b96f77ecedd30abccb
Hidde de Vries <hidde@hiddedevries.nl>
2018-02-14 16:47:43 +0100

    Try Person API if email is not definitely LDAP
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/enter.js
+++ src/js/handlers/enter.js
@@ -5,6 +5,7 @@ module.exports = function enter( element ) {
   var emailField = document.getElementById( 'field-email' );
   var passwordField = document.getElementById( 'field-password' );
   var isDefinitelyLDAP = /mozilla.com|getpocket.com$/.test( emailField.value );
+  var ENDPOINT = 'https://person-api.sso.allizom.org/v1/connection/';
 
   if ( emailField.value === '' || emailField.validity.valid === false ) {
     emailField.focus();
@@ -22,9 +23,33 @@ module.exports = function enter( element ) {
     fireGAEvent( 'Screen change', 'Continued as LDAP' );
   }
   else {
-    // show social logins + passwordless
-    ui.setLockState( element, 'non-ldap' );
-    fireGAEvent( 'Screen change', 'Continued as non-LDAP' );
+    ui.setLockState( form, 'loading' );
+
+    fetch( ENDPOINT + emailField.value )
+      .then(
+        function(response) {
+          response.json().then(function( data ) {
+            var userinfo = JSON.parse( data );
+
+            if ( userinfo.hasOwnProperty( 'user_email' ) && userinfo.hasOwnProperty( 'connection_method' ) ) {
+
+              // show password field
+              ui.setLockState( element, 'ldap' );
+              // focus password field
+              setTimeout( function() {
+                passwordField.focus();
+              }, 400 );
+
+              fireGAEvent( 'Screen change', 'Continued as LDAP' );
+            }
+            else {
+              // show social logins + passwordless
+              ui.setLockState( element, 'non-ldap' );
+              fireGAEvent( 'Screen change', 'Continued as non-LDAP' );
+            }
+          });
+        }
+      );
   }
 };
 

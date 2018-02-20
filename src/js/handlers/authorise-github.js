ae4fb06a6f5c33ccd3a26880018a40e358be2ac2
Hidde de Vries <hidde@hiddedevries.nl>
2018-02-20 15:59:54 +0100

    Save last used connection and attempt to reuse it
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/authorise-github.js
+++ src/js/handlers/authorise-github.js
@@ -1,12 +1,16 @@
 var fireGAEvent = require( 'helpers/fireGAEvent' );
+var storeLastUsedConnection = require( 'helpers/storeLastUsedConnection' );
 
 module.exports = function authorise( element ) {
   var form = element.closest( 'form' );
+  var connection = 'github';
 
   fireGAEvent( 'Authorisation', 'Authorising with GitHub' );
 
   form.webAuth.authorize({
     responseType: 'token',
-    connection: 'github'
+    connection: connection
   });
+
+  storeLastUsedConnection( connection );
 };
--- src/js/handlers/authorise-ldap.js
+++ src/js/handlers/authorise-ldap.js
@@ -1,11 +1,13 @@
 var ui = require( 'helpers/ui' );
 var fireGAEvent = require( 'helpers/fireGAEvent' );
+var storeLastUsedConnection = require( 'helpers/storeLastUsedConnection' );
 
 module.exports = function authorise( element, secondTry ) {
   var form = element.tagName === 'FORM' ? element : element.form;
   var emailField = document.getElementById( 'field-email' );
   var passwordField = secondTry ? document.getElementById( 'field-password-try-2' ) : document.getElementById( 'field-password' );
   var errorText = document.getElementById( 'error-message-ldap' );
+  var connection = 'Mozilla-LDAP-Dev';
 
   if ( element.id === 'authorise-ldap-credentials-try-2' ) {
     passwordField = document.getElementById( 'field-password-try-2' );
@@ -16,7 +18,7 @@ module.exports = function authorise( element, secondTry ) {
   fireGAEvent( 'Authorisation', 'Authorising with LDAP' );
 
   form.webAuth.redirect.loginWithCredentials({
-    connection: 'Mozilla-LDAP-Dev',
+    connection: connection,
     username: emailField.value,
     password: passwordField.value,
     scope: 'openid'
@@ -28,4 +30,6 @@ module.exports = function authorise( element, secondTry ) {
       fireGAEvent( 'Error', 'LDAP: invalid username or password' );
     }
   });
+
+  storeLastUsedConnection( connection );
 };

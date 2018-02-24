7e3b9c99623ea5538f5d3208976b555632c5e847
Hidde de Vries <hidde@hiddedevries.nl>
2018-02-24 16:18:46 +0100

    Call authorise code directly as form submit would fail
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/submit-with-enter.js
+++ src/js/decorators/submit-with-enter.js
@@ -1,10 +1,11 @@
+var authoriseLDAP = require( 'handlers/authorise-ldap' );
+
 module.exports = function( element ) {
   var KEY_ENTER = 13;
-  var form = element.form;
 
   element.addEventListener( 'keyup', function( event ) {
     if ( event.which === KEY_ENTER && element.value.length > 0 ) {
-      form.submit();
+      authoriseLDAP( element.form, false );
     }
   });
 };

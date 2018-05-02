cfa8bc0798ee6b296eb954a11f0237059b888cbb
Hidde de Vries <hidde@hiddedevries.nl>
2018-05-02 10:11:32 +0200

    Lint code;replace nonexisting vars w/ ones that do
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/helpers/autologin.js
+++ src/js/helpers/autologin.js
@@ -3,26 +3,29 @@ var fireGAEvent = require( 'helpers/fire-ga-event' );
 module.exports = function autologin( loginMethod, form ) {
   var visualStatusReport = document.getElementById( 'loading__status' );
   var newLocation;
-  var url = new URL(document.location);
+  var url = new URL( document.location );
   var params = url.searchParams;
 
   form.willRedirect = true;
   visualStatusReport.textContent = 'Attempting auto-login with ' + loginMethod;
 
-  params.set('sso', 'true');
-  params.set('connection', loginMethod);
-  params.set('tried_autologin', 'true');
-  params.set('client_id', myparams.get('client'));
-  params.delete('client');
+  params.set( 'sso', 'true' );
+  params.set( 'connection', loginMethod );
+  params.set( 'tried_autologin', 'true' );
+  params.set( 'client_id', params.get( 'client' ) );
+  params.delete( 'client' );
 
   // Remove 'prompt=&blah=...' where prompt is empty as this confuses OPs that do not support the OPTIONAL `prompt`
   // parameter, such as FxA
-  if (params.get('prompt') === '') {
-    params.delete('prompt');
+  if ( params.get( 'prompt' ) === '' ) {
+    params.delete( 'prompt' );
   }
-  newLocation = url.origin + myurl.pathname.replace( '/login', '/authorize' ) + '?' + params.toString();
+
+  newLocation = url.origin + url.pathname.replace( '/login', '/authorize' ) + '?' + params.toString();
 
   window.location.replace( newLocation );
+
   fireGAEvent( 'Authorisation', 'Performing auto-login with ' + loginMethod );
+
   return;
 };

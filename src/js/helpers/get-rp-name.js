efd0dbb9a1120f65f772e81bdbe00ab2c6407dfc
Hidde de Vries <hidde@hiddedevries.nl>
2018-05-02 13:54:35 +0200

    Also get RP name using searchParams
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/helpers/get-rp-name.js
+++ src/js/helpers/get-rp-name.js
@@ -1,36 +1,13 @@
-function getUrlParameter( name ) {
-  var regex;
-  var results;
+module.exports = function() {
+  var url = new URL( window.location );
+  var redirectURI;
 
-  name = name.replace( /[\[]/, '\\[' ).replace( /[\]]/, '\\]' );
-  regex = new RegExp( '[\\?&]' + name + '=([^&#]*)' );
-  results = regex.exec( location.search );
+  if ( url.searchParams.get( 'redirect_uri' ) ) {
+    redirectURI = new URL( url.searchParams.get( 'redirect_uri' ) );
 
-  return results === null ? '' : decodeURIComponent( results[1].replace( /\+/g, ' ' ) );
-}
-
-// via https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
-function extractHostname( url ) {
-  var hostname;
-
-  // find & remove protocol (http, ftp, etc.) and get hostname
-  if ( url.indexOf( '://' ) > -1 ) {
-    hostname = url.split( '/' )[2];
+    return redirectURI.hostname;
   }
   else {
-    hostname = url.split( '/' )[0];
+    return null;
   }
-
-  // find & remove port number
-  hostname = hostname.split( ':' )[0];
-  // find & remove "?"
-  hostname = hostname.split( '?' )[0];
-
-  return hostname;
-}
-
-module.exports = function() {
-  var RP = getUrlParameter( 'redirect_uri' ) || '';
-
-  return extractHostname( RP );
 };

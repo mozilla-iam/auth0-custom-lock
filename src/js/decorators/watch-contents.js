9bcfb33829ab6ad416299624cd51047a025103cb
Hidde de Vries <hidde@hiddedevries.nl>
2018-05-17 09:56:01 -0700

    More consistent class switching based on input contents
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/watch-contents.js
+++ src/js/decorators/watch-contents.js
@@ -1,14 +1,20 @@
-module.exports = function watchContents( input ) {
+var switchClass = function( input ) {
   if ( input.value.length > 0 ) {
     input.classList.add( 'has-contents' );
   }
+  else {
+    input.classList.remove( 'has-contents' );
+  }
+}
+
+module.exports = function watchContents( input ) {
+  switchClass( input );
 
   input.addEventListener( 'keyup', function() {
-    if ( input.value.length > 0 ) {
-      input.classList.add( 'has-contents' );
-    }
-    else {
-      input.classList.remove( 'has-contents' );
-    }
+    switchClass( input );
+  });
+
+  input.addEventListener( 'change', function() {
+    switchClass( input );
   });
 };

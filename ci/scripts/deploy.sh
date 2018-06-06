616bff054f4a26e7769ab01869ffcbdc01679e9e
andrewkrug <akrug@mozilla.com>
2018-06-06 15:34:52 -0700

    update exports of lock vars to global instead of local scope

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -11,10 +11,10 @@ source env/bin/activate
 pip3 install auth0-ci
 pip3 install awscli
 
-CLIENT_ID=$(aws ssm get-parameters --names /iam/nlx/deploy_client_id --query Parameters[0].Value)
-CLIENT_SECRET=$(aws ssm get-parameters --names /iam/nlx/deploy_client_secret --with-decryption --query Parameters[0].Value)
-LOCK_CLIENT_ID=$(aws ssm get-parameters --names /iam/nlx/lock_client_id --query Parameters[0].Value)
-AUTH0_URL=$(aws ssm get-parameters --names /iam/nlx/auth0_url --query Parameters[0].Value)
+export CLIENT_ID=$(aws ssm get-parameters --names /iam/nlx/deploy_client_id --query Parameters[0].Value)
+export CLIENT_SECRET=$(aws ssm get-parameters --names /iam/nlx/deploy_client_secret --with-decryption --query Parameters[0].Value)
+export LOCK_CLIENT_ID=$(aws ssm get-parameters --names /iam/nlx/lock_client_id --query Parameters[0].Value)
+export AUTH0_URL=$(aws ssm get-parameters --names /iam/nlx/auth0_url --query Parameters[0].Value)
 
 # Actually deploy the lock
 python3 env/bin/uploader_login_page.py -u $AUTH0_URL -c $CLIENT_ID -s $CLIENT_SECRET --default-client $LOCK_CLIENT_ID --login-page dist/index.html

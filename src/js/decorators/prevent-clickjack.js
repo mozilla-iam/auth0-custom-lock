a8cb6ef25a0ac0a7a627b26bad519b84ffa6f787
Hidde de Vries <hidde@hiddedevries.nl>
2018-06-19 16:58:38 +0200

    Hide body when in frame
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/prevent-clickjack.js
+++ src/js/decorators/prevent-clickjack.js
@@ -1,16 +1,11 @@
 var fireGAEvent = require( 'helpers/fire-ga-event' );
 
 module.exports = function() {
-  console.log( 'self: ');
-  console.log( window.self );
-  console.log( 'top: ' );
-  console.log( window.top );
   if ( window.self !== window.top ) {
-    document.body.classList.add( 'pppppp' );
     document.body.style.display = 'none';
     fireGAEvent( 'Security', 'NLX was loaded inside iframe' );
   }
   if ( window.self === window.top ) {
     document.body.classList.add( 'notframed' );
   }
-}
+};

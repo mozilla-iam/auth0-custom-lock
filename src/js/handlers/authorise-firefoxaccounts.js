f0edbc722dbcf28b302a222625e46ed77ae1d483
Hidde de Vries <hidde@hiddedevries.nl>
2018-06-26 17:46:02 +0200

    Undo prompt changes
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/handlers/authorise-firefoxaccounts.js
+++ src/js/handlers/authorise-firefoxaccounts.js
@@ -4,19 +4,8 @@ var storeLastUsedConnection = require( 'helpers/store-last-used-connection' );
 module.exports = function authorise( element ) {
   var form = element.closest( 'form' );
   var connection = 'firefoxaccounts';
-  var url = window.location;
-  var params = new URLSearchParams( url.search );
-  var newLocation;
 
   fireGAEvent( 'Authorisation', 'Authorising with Firefox Accounts' );
-
-  if ( params.get( 'prompt' ) ) {
-    params.delete( 'prompt' );
-  }
-
-  newLocation = url.origin + url.pathname + '?' + params.toString();
-
-  window.history.replaceState( null, null, newLocation );
 
   form.webAuth.authorize({
     responseType: 'token',

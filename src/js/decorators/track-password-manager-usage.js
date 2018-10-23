63f26e87408f247528a5012cc67b63a6e7e1e1c3
Hidde de Vries <hidde@hiddedevries.nl>
2018-10-23 11:45:01 +0200

    Remove console log
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/js/decorators/track-password-manager-usage.js
+++ src/js/decorators/track-password-manager-usage.js
@@ -19,7 +19,6 @@ module.exports = function trackPasswordManagerUsage() {
     }
 
     end = Date.now();
-    console.log("Handler for .keypress() called. " + event.which);
   }
 
   function report() {
